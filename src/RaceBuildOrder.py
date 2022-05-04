19ec73af46f7440f748ca92d67302db50253851b
sav <christopher.savini@gmail.com>
2022-05-05 01:51:06 +1000

    RaceBuildOrder and test cases working

--- src/RaceBuildOrder.py
+++ src/RaceBuildOrder.py
@@ -13,50 +13,51 @@ class Race(Enum):
 
 BUILD_ORDER: Type[List[int]] = List[int] 
 BUILD_ORDER_STR: Type[List[str]] = List[str] 
+BUILD_ORDER_CHR: Type[List[str]] = List[str] 
 
 
-def levenshtein_distance_metric(left: BUILD_ORDER, right: BUILD_ORDER)->int:
+def levenshtein_distance_metric(left: BUILD_ORDER_CHR, right: BUILD_ORDER_CHR)->int:
     return Levenshtein.distance(left, right)
 
-class RaceBuildOrder:
-    VersusTerran  : Type[List[BUILD_ORDER]] = [] 
-    VersusZerg    : Type[List[BUILD_ORDER]] = []
-    VersusProtoss : Type[List[BUILD_ORDER]] = []
 
-    Race : Race = Race.Terran
-    Label_Encoder: LabelEncoder = LabelEncoder()
-    
-    TerranLevenshteinMatrix  : np.array = np.zeros(0)
-    ZergLevenshteinMatrix    : np.array = np.zeros(0)
-    ProtossLevenshteinMatrix : np.array = np.zeros(0)
+class RaceBuildOrder:
 
     def __init__(self, Race: Race):
+        self.VersusTerran: Type[List[BUILD_ORDER]] = []
+        self.VersusZerg: Type[List[BUILD_ORDER]] = []
+        self.VersusProtoss: Type[List[BUILD_ORDER]] = []
+
+        self.Label_Encoder: LabelEncoder = LabelEncoder()
+
+        self.TerranLevenshteinMatrix: np.array = np.zeros(0)
+        self.ZergLevenshteinMatrix: np.array = np.zeros(0)
+        self.ProtossLevenshteinMatrix: np.array = np.zeros(0)
+
         self.Race = Race
         self.Label_Encoder.fit([])
 
-    def add_build_order(self, bo : BUILD_ORDER, bos: List[BUILD_ORDER]):
-        bos.append(bo)
+    def add_build_order(self, bo : BUILD_ORDER_STR, bos: List[BUILD_ORDER_STR]):
+        self.build_labels(bo)
+        bos.append(self.Label_Encoder.transform(bo))
 
-    def add_terran_build_order(self, bo : BUILD_ORDER):
+    def add_terran_build_order(self, bo : BUILD_ORDER_STR):
         self.add_build_order(bo, self.VersusTerran)
 
-    def add_zerg_build_order(self, bo : BUILD_ORDER):
+    def add_zerg_build_order(self, bo : BUILD_ORDER_STR):
         self.add_build_order(bo, self.VersusZerg)
 
-    def add_protoss_build_order(self, bo : BUILD_ORDER):
+    def add_protoss_build_order(self, bo : BUILD_ORDER_STR):
         self.add_build_order(bo, self.VersusProtoss)
 
 
     def compute_levenshtein_matrix(self, levenshtein_matrix: np.array, build_orders : Type[List[BUILD_ORDER]]):
         length = len(build_orders)
-        #TO DO: Maybe do ascii conversion in here?
-
         # Populate Upper triangular and mirror on lower triangular, diagonal stays zero
         for i in range(0, length):
             for j in range(i+1, length):
-                print('%d, %d' % (i,j))
-                left = build_orders[i]
-                right = build_orders[j]
+                #TO DO: Possible optimisation here? 
+                left = [chr(letter) for letter in build_orders[i]] 
+                right = [chr(letter) for letter in build_orders[j]]
                 levenshtein_matrix[i,j] = levenshtein_distance_metric(left, right)
                 levenshtein_matrix[j,i] = levenshtein_matrix[i,j]
 
@@ -64,13 +65,13 @@ class RaceBuildOrder:
 
         tLength = len(self.VersusTerran)
         zLength = len(self.VersusZerg)
-        pLength = len(self.VersusTerran)
-        self.TerranLevenshteinMatrix = np.zeros((tLength, tLength)) 
-        self.ZergLevenshteinMatrix   = np.zeros((zLength, zLength)) 
-        self.ZergLevenshteinMatrix   = np.zeros((pLength, pLength)) 
+        pLength = len(self.VersusProtoss)
+        self.TerranLevenshteinMatrix  = np.zeros((tLength, tLength)) 
+        self.ZergLevenshteinMatrix    = np.zeros((zLength, zLength)) 
+        self.ProtossLevenshteinMatrix = np.zeros((pLength, pLength)) 
 
-        self.compute_levenshtein_matrix(self.TerranLevenshteinMatrix, self.VersusTerran)
-        self.compute_levenshtein_matrix(self.ZergLevenshteinMatrix, self.VersusZerg)
+        self.compute_levenshtein_matrix(self.TerranLevenshteinMatrix,  self.VersusTerran)
+        self.compute_levenshtein_matrix(self.ZergLevenshteinMatrix,    self.VersusZerg)
         self.compute_levenshtein_matrix(self.ProtossLevenshteinMatrix, self.VersusProtoss)
 
 
@@ -79,4 +80,8 @@ class RaceBuildOrder:
 
     def decode_labels(self, build_order: BUILD_ORDER)->BUILD_ORDER_STR:
         return self.Label_Encoder.inverse_transform(build_order)
+
+    
+
+
 
--- test/RaceBuildOrder.tests.py
+++ test/RaceBuildOrder.tests.py
@@ -17,7 +17,7 @@ class TestRaceBuildOrder(unittest.TestCase):
         race_build_order : RaceBuildOrder.RaceBuildOrder = RaceBuildOrder.RaceBuildOrder(RaceBuildOrder.Race.Zerg)
 
         self.assertEqual(len(race_build_order.VersusTerran), 0)
-        test_build_order: RaceBuildOrder.BUILD_ORDER = [1, 2, 3, 4]
+        test_build_order: RaceBuildOrder.BUILD_ORDER_STR = ['Drone', 'Drone', 'Hatchery', 'Drone']
         race_build_order.add_terran_build_order(test_build_order) 
         self.assertEqual(len(race_build_order.VersusTerran), 1)
 
@@ -25,7 +25,7 @@ class TestRaceBuildOrder(unittest.TestCase):
         race_build_order : RaceBuildOrder.RaceBuildOrder = RaceBuildOrder.RaceBuildOrder(RaceBuildOrder.Race.Zerg)
 
         self.assertEqual(len(race_build_order.VersusZerg), 0)
-        test_build_order: RaceBuildOrder.BUILD_ORDER = [1, 2, 3, 4]
+        test_build_order: RaceBuildOrder.BUILD_ORDER_STR = ['Drone', 'Drone', 'Hatchery', 'Drone']
         race_build_order.add_zerg_build_order(test_build_order) 
         self.assertEqual(len(race_build_order.VersusZerg), 1)
 
@@ -33,7 +33,7 @@ class TestRaceBuildOrder(unittest.TestCase):
         race_build_order : RaceBuildOrder.RaceBuildOrder = RaceBuildOrder.RaceBuildOrder(RaceBuildOrder.Race.Zerg)
 
         self.assertEqual(len(race_build_order.VersusProtoss), 0)
-        test_build_order: RaceBuildOrder.BUILD_ORDER = [1, 2, 3, 4]
+        test_build_order: RaceBuildOrder.BUILD_ORDER_STR = ['Drone', 'Drone', 'Hatchery', 'Drone']
         race_build_order.add_protoss_build_order(test_build_order) 
         self.assertEqual(len(race_build_order.VersusProtoss), 1)
     
@@ -48,7 +48,29 @@ class TestRaceBuildOrder(unittest.TestCase):
         numpy.testing.assert_array_equal(race_build_order.decode_labels(test_build_order), np.array(test_lables3))
 
     def test_compute_levenshtein_matrices(self):
+        race_build_order : RaceBuildOrder.RaceBuildOrder = RaceBuildOrder.RaceBuildOrder(RaceBuildOrder.Race.Protoss)
+
+        test_lables1 : RaceBuildOrder.BUILD_ORDER_STR = ['Probe', 'Probe', 'Pylon', 'Gateway', 'Nexus', 'Cybernetics Core', 'Assimliator']
+        test_lables2 : RaceBuildOrder.BUILD_ORDER_STR = ['Probe', 'Probe', 'Pylon', 'Gateway', 'Gateway', 'Cybernetics Core', 'Assimliator', 'Pylon']
+        test_lables3 : RaceBuildOrder.BUILD_ORDER_STR = ['Probe', 'Probe', 'Pylon', 'Forge', 'Pylon', 'Probe', 'Photon Cannon', 'Pylon']
+
+        race_build_order.add_protoss_build_order(test_lables1)
+        race_build_order.add_protoss_build_order(test_lables2)
+        race_build_order.add_protoss_build_order(test_lables3)
+
+        pLength = len(race_build_order.VersusProtoss)
+        race_build_order.ProtossLevenshteinMatrix = np.zeros((pLength, pLength))
         
+        race_build_order.compute_levenshtein_matrix(race_build_order.ProtossLevenshteinMatrix, race_build_order.VersusProtoss)
+
+        #check symmetry 
+        self.assertTrue(np.allclose(race_build_order.ProtossLevenshteinMatrix, race_build_order.ProtossLevenshteinMatrix.T))
+
+        #Check 3 x 3
+        self.assertTrue((race_build_order.ProtossLevenshteinMatrix.shape == np.array([3,3])).all())
+
+        #Check diagonal is all zeros 
+        self.assertFalse(race_build_order.ProtossLevenshteinMatrix.diagonal().any())
 
 
 if __name__ == '__main__':
