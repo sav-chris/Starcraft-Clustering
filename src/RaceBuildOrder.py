9a515c17546d1184ec83efeb4a69584d6ded2160
sav <christopher.savini@gmail.com>
2022-05-05 00:20:24 +1000

    Refactor and Unit tests

---
+++ src/RaceBuildOrder.py
@@ -0,0 +1,82 @@
+from typing import List
+from typing import Type
+import numpy as np
+from numpy import array
+from enum import Enum
+from sklearn.preprocessing import LabelEncoder
+from rapidfuzz.distance import Levenshtein
+
+class Race(Enum):
+    Terran = 1
+    Zerg = 2
+    Protoss = 3
+
+BUILD_ORDER: Type[List[int]] = List[int] 
+BUILD_ORDER_STR: Type[List[str]] = List[str] 
+
+
+def levenshtein_distance_metric(left: BUILD_ORDER, right: BUILD_ORDER)->int:
+    return Levenshtein.distance(left, right)
+
+class RaceBuildOrder:
+    VersusTerran  : Type[List[BUILD_ORDER]] = [] 
+    VersusZerg    : Type[List[BUILD_ORDER]] = []
+    VersusProtoss : Type[List[BUILD_ORDER]] = []
+
+    Race : Race = Race.Terran
+    Label_Encoder: LabelEncoder = LabelEncoder()
+    
+    TerranLevenshteinMatrix  : np.array = np.zeros(0)
+    ZergLevenshteinMatrix    : np.array = np.zeros(0)
+    ProtossLevenshteinMatrix : np.array = np.zeros(0)
+
+    def __init__(self, Race: Race):
+        self.Race = Race
+        self.Label_Encoder.fit([])
+
+    def add_build_order(self, bo : BUILD_ORDER, bos: List[BUILD_ORDER]):
+        bos.append(bo)
+
+    def add_terran_build_order(self, bo : BUILD_ORDER):
+        self.add_build_order(bo, self.VersusTerran)
+
+    def add_zerg_build_order(self, bo : BUILD_ORDER):
+        self.add_build_order(bo, self.VersusZerg)
+
+    def add_protoss_build_order(self, bo : BUILD_ORDER):
+        self.add_build_order(bo, self.VersusProtoss)
+
+
+    def compute_levenshtein_matrix(self, levenshtein_matrix: np.array, build_orders : Type[List[BUILD_ORDER]]):
+        length = len(build_orders)
+        #TO DO: Maybe do ascii conversion in here?
+
+        # Populate Upper triangular and mirror on lower triangular, diagonal stays zero
+        for i in range(0, length):
+            for j in range(i+1, length):
+                print('%d, %d' % (i,j))
+                left = build_orders[i]
+                right = build_orders[j]
+                levenshtein_matrix[i,j] = levenshtein_distance_metric(left, right)
+                levenshtein_matrix[j,i] = levenshtein_matrix[i,j]
+
+    def compute_levenshtein_matrices(self):
+
+        tLength = len(self.VersusTerran)
+        zLength = len(self.VersusZerg)
+        pLength = len(self.VersusTerran)
+        self.TerranLevenshteinMatrix = np.zeros((tLength, tLength)) 
+        self.ZergLevenshteinMatrix   = np.zeros((zLength, zLength)) 
+        self.ZergLevenshteinMatrix   = np.zeros((pLength, pLength)) 
+
+        self.compute_levenshtein_matrix(self.TerranLevenshteinMatrix, self.VersusTerran)
+        self.compute_levenshtein_matrix(self.ZergLevenshteinMatrix, self.VersusZerg)
+        self.compute_levenshtein_matrix(self.ProtossLevenshteinMatrix, self.VersusProtoss)
+
+
+    def build_labels(self, build_order: BUILD_ORDER_STR):
+        self.Label_Encoder.fit(np.append(self.Label_Encoder.classes_, array(build_order)))
+
+    def decode_labels(self, build_order: BUILD_ORDER)->BUILD_ORDER_STR:
+        return self.Label_Encoder.inverse_transform(build_order)
+
--- test/RaceBuildOrder.tests.py
+++ test/RaceBuildOrder.tests.py
@@ -1,30 +1,57 @@
 import sys
 import os
-from pathlib import Path
 import unittest
-#from ..src.RaceBuildOrder import RaceBuildOrder
-
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-ROOT_DIR : str = Path(SCRIPT_DIR).parent.absolute()
-SRC_DIR  : str = os.path.join(ROOT_DIR, 'src')
-
-print(SCRIPT_DIR)
-print(ROOT_DIR)
-print(SRC_DIR)
-
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
 import RaceBuildOrder
-#sys.path.append(os.path.dirname(SRC_DIR))
-
-#from RaceBuildOrder import RaceBuildOrder
+import numpy.testing
+import numpy as np
 
 
 class TestRaceBuildOrder(unittest.TestCase):
 
     def test_set_race(self):
-        race_build_order : RaceBuildOrder.RaceBuildOrder = RaceBuildOrder.RaceBuildOrder(RaceBuildOrder.Race.Terran)
-        self.assertEqual(race_build_order.Race, race_build_order.Race.Terran)
+        race_build_order : RaceBuildOrder.RaceBuildOrder = RaceBuildOrder.RaceBuildOrder(RaceBuildOrder.Race.Zerg)
+        self.assertEqual(race_build_order.Race, race_build_order.Race.Zerg)
+
+    def test_add_terran_build_order(self):
+        race_build_order : RaceBuildOrder.RaceBuildOrder = RaceBuildOrder.RaceBuildOrder(RaceBuildOrder.Race.Zerg)
+
+        self.assertEqual(len(race_build_order.VersusTerran), 0)
+        test_build_order: RaceBuildOrder.BUILD_ORDER = [1, 2, 3, 4]
+        race_build_order.add_terran_build_order(test_build_order) 
+        self.assertEqual(len(race_build_order.VersusTerran), 1)
+
+    def test_add_zerg_build_order(self):
+        race_build_order : RaceBuildOrder.RaceBuildOrder = RaceBuildOrder.RaceBuildOrder(RaceBuildOrder.Race.Zerg)
+
+        self.assertEqual(len(race_build_order.VersusZerg), 0)
+        test_build_order: RaceBuildOrder.BUILD_ORDER = [1, 2, 3, 4]
+        race_build_order.add_zerg_build_order(test_build_order) 
+        self.assertEqual(len(race_build_order.VersusZerg), 1)
+
+    def test_add_protoss_build_order(self):
+        race_build_order : RaceBuildOrder.RaceBuildOrder = RaceBuildOrder.RaceBuildOrder(RaceBuildOrder.Race.Zerg)
+
+        self.assertEqual(len(race_build_order.VersusProtoss), 0)
+        test_build_order: RaceBuildOrder.BUILD_ORDER = [1, 2, 3, 4]
+        race_build_order.add_protoss_build_order(test_build_order) 
+        self.assertEqual(len(race_build_order.VersusProtoss), 1)
+    
+    def test_build_labels(self):
+        race_build_order : RaceBuildOrder.RaceBuildOrder = RaceBuildOrder.RaceBuildOrder(RaceBuildOrder.Race.Zerg)
+        test_lables1 : RaceBuildOrder.BUILD_ORDER_STR = ['Drone', 'Drone', 'Spawning Pool', 'Zergling']
+        test_lables2 : RaceBuildOrder.BUILD_ORDER_STR = ['Drone', 'Extractor', 'Hatchery', 'Zergling']
+        race_build_order.build_labels(test_lables1)
+        race_build_order.build_labels(test_lables2)
+        test_lables3 : RaceBuildOrder.BUILD_ORDER_STR = ['Spawning Pool', 'Drone', 'Extractor']
+        test_build_order: RaceBuildOrder.BUILD_ORDER = race_build_order.Label_Encoder.transform(test_lables3)
+        numpy.testing.assert_array_equal(race_build_order.decode_labels(test_build_order), np.array(test_lables3))
+
+    def test_compute_levenshtein_matrices(self):
+        
+
 
 if __name__ == '__main__':
     unittest.main()
+
+    
